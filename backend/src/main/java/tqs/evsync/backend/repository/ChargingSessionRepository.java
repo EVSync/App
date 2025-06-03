8cace49a0cf38d217c772a63007f2f3104ba0a61
Filipe Oliveira <filiperoliveira@ua.pt>
2025-06-03 15:43:13 +0100

    merge

--- backend/src/main/java/tqs/evsync/backend/repository/ChargingSessionRepository.java
+++
@@ -1,8 +0,0 @@
-package tqs.evsync.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tqs.evsync.backend.model.ChargingSession;
-
-public interface ChargingSessionRepository 
-        extends JpaRepository<ChargingSession,Long> {
-}
